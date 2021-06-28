5c10f0d3e994a2c89157046b5406655f9a82b177
josephmartin721 <josephmartin721@yahoo.com>
2021-06-27 22:28:09 -0500

    working package scripts and Nav folder components

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const path = requires("path");
 const routes = require("./routes");
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
 // Define middleware
 app.use(express.urlencoded({ extended: true }));
