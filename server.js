bc43ec0008e6056b9612a6ddea7f1acd0d86eeb7
josephmartin721 <josephmartin721@yahoo.com>
2021-07-24 12:15:58 -0500

    server

--- server.js
+++ server.js
@@ -1,23 +1,30 @@
-const app = express();
 const express = require("express");
-const mongoose = require("mongoose");
-const path = requires("path");
+const path = require("path");
 const routes = require("./routes");
-const PORT = process.env.PORT || 3000;
+const mongoose = require("mongoose");
+const PORT = process.env.PORT || 3001;
+const app = express();
 
-// Define middleware
+// Middleware definition
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// Static assets
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
-// Define API
-app.use("/api", routes);
+// Connect to the Mongo DB
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/googlebooks",
+  { useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true }
+);
 
-//Connect to MongoDb
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks", { useNewUrlParser: true });
+// Define API routes here
+app.use(routes);
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
 
 app.listen(PORT, () => {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
