1a6c769d4ce3f711c597b3f6f5c2682f426cd946
josephmartin721 <josephmartin721@yahoo.com>
2021-07-24 12:28:27 -0500

    server

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ if (process.env.NODE_ENV === "production") {
 
 // Connect to the Mongo DB
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/googlebooks",
+  process.env.MONGODB_URI || "mongodb://localhost/jrm-googlebooksearch",
   { useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true }
 );
 
